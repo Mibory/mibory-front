e1da0b048f3d176e30b6db4bc968e6df7b142277
Michał Tarnowski <michal.tarnowski03@gmail.com>
2026-03-17 11:15:26 +0100

    Refactor TrainingDay component to improve exercise rendering and data fetching logic

--- src/lib/views/TrainingDay/TrainingDay.tsx
+++ src/lib/views/TrainingDay/TrainingDay.tsx
@@ -1,33 +1,80 @@
 import './TrainingDay.css';
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button, ButtonVariants } from "../../components/Button/Button";
 import { SquareButton } from "../../components/SquareButton/SquareButton";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 
 const dayName = [
     'dupcia', 'brzusio', 'góra'
 ] as const;
 
+
+type TTrainingDay = {
+    _id: string;
+    dayNum: number;
+    user: {
+        _id: string;
+        name: string;
+        currentTrainingDay: number;
+    };
+    exercises: {
+        _id: string;
+        name: string;
+        videoUrl: string;
+    }[];
+};
+
 export function TrainingDay() {
 
-    const navigator = useNavigate();
+    const navigate = useNavigate();
     const [dayNum, setDayNum] = useState(1);
+    const [trainingDays, setTrainingDays] = useState<TTrainingDay[]>([]);
+    const [searchParams] = useSearchParams();
+    const userName = searchParams.get('usr');
+
+    useEffect(() => {
+        const cachedData = sessionStorage.getItem('cached_training_days');
+        if (cachedData) {
+            setTrainingDays(JSON.parse(cachedData));
+            return;
+        }
+
+        fetch(`/api/training-days?usr=${userName}`, {
+            method: "GET",
+            headers: { "Content-Type": "application/json" }
+        })
+            .then(res => res.json())
+            .then(data => {
+                setTrainingDays(data);
+                sessionStorage.setItem('cached_training_days', JSON.stringify(data));
+            })
+            .catch(err => console.error('Fetch error: ', err));
+    }, []);
+
     return (
         <>
             <div className="day-num-container">
                 <h1 className="text-[42px]">Day <span className="font-bold text-[36px] text-orange">{dayName[dayNum - 1]}</span></h1>
             </div>
             <div className="day-excercises-container">
-                <Button text="bench press" onClick={() => navigator(`/excercise?name=${encodeURIComponent('bench press')}`)} className="capitalize" />
-                <Button text="pull up" onClick={() => navigator(`/excercise?name=${encodeURIComponent('pull up')}`)} variant={ButtonVariants.lightblue} className="capitalize" />
-                <Button text="ławeczka" onClick={() => { }} className="capitalize" />
-                <Button text="biceps" onClick={() => { }} variant={ButtonVariants.lightblue} className="capitalize" />
-                <Button text="triceps" onClick={() => { }} className="capitalize" />
-                <Button text="łydka" onClick={() => { }} variant={ButtonVariants.lightblue} className="capitalize" />
+                {
+                    trainingDays
+                        .find(td => td.dayNum === dayNum)
+                        ?.exercises
+                        .map((ex, id) => (
+                            <Button
+                                key={ex._id}
+                                text={ex.name}
+                                onClick={() => navigate(`/exercise?name=${encodeURIComponent(ex.name)}&url=${encodeURIComponent(ex.videoUrl)}`)}
+                                className='capitalize'
+                                variant={id % 2 ? ButtonVariants.lightblue : undefined}
+                            />
+                        ))
+                }
             </div>
             <div className="action-btns-container">
-                <SquareButton text="other" onClick={() => navigator('/exercise-browser')} variant={ButtonVariants.orange} />
+                <SquareButton text="other" onClick={() => navigate('/exercise-browser')} variant={ButtonVariants.orange} />
                 <SquareButton text="skip" onClick={() => {
                     setDayNum((dayNum % 3) + 1);
                 }} />
